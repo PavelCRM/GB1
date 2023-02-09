0872abd9f4cbf9abdf115d08688292d9d3151621
PavelCRM <nachtmahr666@yandex.ru>
2023-02-09 22:38:00 +0300

    Ispravlenie nedochetov

--- Notes/app.py
+++ Notes/app.py
@@ -5,26 +5,26 @@ from modelJSON import ModelJSON
 from note import Note
 from view import View
 
+
 def run():
     c = Controller(ModelJSON("notes.json"), View())
 
     while True:
         command = input(
-            '1 - Создать заметку\n'
-            '2 - Прочитать заметку\n'
-            '3 - Обновить заметку\n'
-            '4 - Удалить заметку\n'
-            '5 - Удалить все заметки\n'
-            '6 - Прочитать все заметки\n'
-            '7 - Выход\n' +
-            'Сделайте ваш выбор: '
-        )
-        if command =='7':
-           break
+            '1 - создать заметку\n'
+            '2 - прочитать заметку\n'
+            '3 - обновить заметку\n'
+            '4 - удалить заметку\n'
+            '5 - удалить все заметки\n'
+            '6 - прочитать все заметки\n'
+            '7 - выход\n' +
+            'Сделайте Ваш выбор: ')
+        if command == '7':
+            break
 
-        if command =='1'
-           print('\nСоздать заметку:')
-           с.create_note(get_note_data())
+        if command == '1':
+            print('\nСоздать заметку:')
+            c.create_note(get_note_data())
 
         elif command == '2':
             print('\nПрочитать заметку:')
@@ -33,40 +33,44 @@ def run():
         elif command == '3':
             if c.notes_exist():
                 print('\nОбновить заметку:')
-                update_id = int(get number())
+                updated_id = int(get_number())
                 if c.note_id_exist(updated_id):
                     c.update_note(updated_id, get_note_data())
+
         elif command == '4':
-            if c.notes_exist():            
+            if c.notes_exist():
                 print('\nУдалить заметку:')
                 delete_id = int(get_number())
                 if c.note_id_exist(delete_id):
                     c.delete_note(delete_id)
+
         elif command == '5':
-            if c.notes_exist():            
+            if c.notes_exist():
                 print('\nУдалить все заметки:')
-                if input('Вы точно хотите удалить все заметки? (Y/N):').capitalize() == 'Y':
-                   if c.note_id_exist():
-                       c.delete_all_notes()
+                if input('Вы точно хотите удалить все заметки? (Y/N): ').capitalize() == 'Y':
+                    if c.notes_exist():
+                        c.delete_all_notes()
+
         elif command == '6':
-            if c.notes_exist():            
+            if c.notes_exist():
                 print('\nСписок всех заметок:')
-                с.show_notes()
+                c.show_notes()
         else:
             print('Команда не найдена')
 
+
 def get_note_data():
-    note_id =0
+    note_id = 0
     date = datetime.datetime.now()
     title = input('Введите заголовок заметки: ')
-    text = input('Введите содержание заметки: ') 
-    return Note(note_id, date, title, text) 
+    text = input('Введите тело заметки: ')
+    return Note(note_id, date, title, text)
+
 
 def get_number():
     while True:
         get_choice = input('Введите id заметки: ')
-        if get_choice/isdigit() and int(get_choice) > 0:
+        if get_choice.isdigit() and int(get_choice) > 0:
             return get_choice
         else:
-            print('Введите целое положительное число')
-                                                                 
+            print('Введите целое положительное число!')        
